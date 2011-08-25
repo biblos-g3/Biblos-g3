d564955b814032d2ed6a4e860e02dad448846406
Nunhez clase <nunhez82@hotmail.com>
2011-08-25 10:59:15 +0200

    Primer intento con altacatalogo

--- altaCatalogoG.php
+++ altaCatalogoG.php
@@ -11,20 +11,35 @@ $usuario = $_SESSION['usuario'];
     </head>
     <body>
         <form action="altaCatalogoP.php" method="post">
-            Categoria Dewey <input name="dewey" type="text" size="20" maxlength="20" /><br />
-            Nombre título<input name="nombre_titulo" type="text" size="20" maxlength="20" /><br />
+            <?php
+            echo selectorDewey();
+            ?>
+            <label>Nombre t&iacute;tulo</label><input name="nombre_titulo" type="text" size="20" maxlength="20" /><br />
+            Autor <input name="autor" type="text" size="20" maxlength="20" /><br />
             ISBN <input name="isbn" type="text" size="20" maxlength="20" /><br />
             Fecha publicación <input name="fecha_publicacion" type="text" size="20" maxlength="20" /><br />
             Fecha adquisición <input name="fecha_adquisicion" type="text" size="20" maxlength="20" /><br />
             Sinopsis <input name="sinopsis" type="text" size="20" maxlength="20" /><br />
             Número de páginas<input name="num_paginas" type="text" size="20" maxlength="20" /><br />
             Edición <input name="edicion" type="text" size="20" maxlength="20" /><br />
+            Editorial <input name="editorial" type="text" size="20" maxlength="20" /><br />
             Idioma<input name="idioma" type="text" size="20" maxlength="20" /><br />
             <input type="submit" value="Dar de alta">
             <input type="reset" value="Borrar campos">
         </form>
 
+
         <?php
+
+        function selectorDewey() {
+            iniciaBD();
+            $query = "SELECT * FROM dewey";
+            $resultado = mysql_query($query);
+            echo "<select name='dewey' size='1' >";
+            while ($dewey = mysql_fetch_array($resultado)) {
+                    echo "<option  value='" . $dewey['categoria_dewey'] . "'>" . htmlentities($dewey['nombre_categoria_dewey']) . "</option>";              
+            }
+        }
         ?>
     </body>
 </html>
