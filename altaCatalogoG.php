9f2aa14fd212d55b173fb8ec480d96c2a2938eb4
Nunhez clase <nunhez82@hotmail.com>
2011-08-25 12:58:39 +0200

    Formulario alta catalogo completo

--- altaCatalogoG.php
+++ altaCatalogoG.php
@@ -7,38 +7,58 @@ $usuario = $_SESSION['usuario'];
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title> Alta de Catalogo</title>
+        <title> Alta de Cat&aacute;logo</title>
     </head>
     <body>
         <form action="altaCatalogoP.php" method="post">
+            <fieldset>            
+                <legend>Alta Cat&aacute;logo</legend>
+            <label>Categoria Dewey</label><br />
             <?php
-            echo selectorDewey();
+            cargardorLista("dewey", "categoria_dewey ", "nombre_categoria_dewey", 10)
             ?>
             <label>Nombre t&iacute;tulo</label><input name="nombre_titulo" type="text" size="20" maxlength="20" /><br />
-            Autor <input name="autor" type="text" size="20" maxlength="20" /><br />
-            ISBN <input name="isbn" type="text" size="20" maxlength="20" /><br />
-            Fecha publicación <input name="fecha_publicacion" type="text" size="20" maxlength="20" /><br />
-            Fecha adquisición <input name="fecha_adquisicion" type="text" size="20" maxlength="20" /><br />
-            Sinopsis <input name="sinopsis" type="text" size="20" maxlength="20" /><br />
-            Número de páginas<input name="num_paginas" type="text" size="20" maxlength="20" /><br />
-            Edición <input name="edicion" type="text" size="20" maxlength="20" /><br />
-            Editorial <input name="editorial" type="text" size="20" maxlength="20" /><br />
-            Idioma<input name="idioma" type="text" size="20" maxlength="20" /><br />
-            <input type="submit" value="Dar de alta">
+            <label>Autor</label>
+            <?php
+            cargardorLista("autor", "id_autor", "nombre_autor");
+            ?>
+            <label>ISBN</label>
+            <input name="isbn" type="text" size="20" maxlength="20" /><br />
+            <label>Fecha publicaci&oacute;n</label>
+            <input name="fecha_publicacion" type="text" size="20" maxlength="20" /><br />
+            <label>Fecha adquisición</label>
+            <input name="fecha_adquisicion" type="text" size="20" maxlength="20" /><br />
+            <label>Sinopsis</label><br />
+            <textarea name="sinopsis" cols=40 rows=6></textarea><br />
+            <label>Número de páginas</label>
+            <input name="num_paginas" type="text" size="10" maxlength="10" /><br />
+            <label>Edición</label>
+            <input name="edicion" type="text" size="10" maxlength="10" /><br />
+            <label>Editorial</label>
+            <?php
+            cargardorLista("editorial", "id_editorial", "nombre_editorial");
+            ?>
+            <label>Idioma</label>
+            <?php
+            cargardorLista("idioma_639_1", "id_idioma_639_1", "idioma_639_1")
+            ?>
+            <br /><input type="submit" value="Dar de alta">
             <input type="reset" value="Borrar campos">
+            </fieldset>
         </form>
-
 
         <?php
 
-        function selectorDewey() {
+        function cargardorLista($nombreTabla, $codCampo, $valorCampo, $visibles=1) {
             iniciaBD();
-            $query = "SELECT * FROM dewey";
+            $query = "SELECT * FROM $nombreTabla";
             $resultado = mysql_query($query);
-            echo "<select name='dewey' size='1' >";
-            while ($dewey = mysql_fetch_array($resultado)) {
-                    echo "<option  value='" . $dewey['categoria_dewey'] . "'>" . htmlentities($dewey['nombre_categoria_dewey']) . "</option>";              
+
+            echo "<select name='dewey' size='$visibles'>";
+            while ($salida = mysql_fetch_array($resultado)) {
+                echo "<option  value='" . $salida[$codCampo] . "'>" . htmlentities($salida[$valorCampo]) . "</option>";
             }
+            echo "</select><br />";
         }
         ?>
     </body>
